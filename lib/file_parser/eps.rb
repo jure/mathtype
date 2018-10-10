9613f0fc07275fc3dcf36a8873c5a16d63eb96ba
Sebastian Bulka <sbulka@le-tex.de>
2018-10-10 16:59:52 +0200

    react to string with other comment-count than !1!1!

--- lib/file_parser/eps.rb
+++ lib/file_parser/eps.rb
@@ -6,12 +6,20 @@ module Mathtype
     end
 
     def extract_mtef_from_epscomment(comment)
-      string64 = @raw.gsub /%|\r|\n/, ""
-      string64 = string64.gsub /.*MathType!MTEF!1!1!\+-(.*![0-9A-F]{4}!).*/, '\\1'
-      mtchecksum = string64[-5 .. -2]
-      string64 = string64[ 0 .. -7]
+      string64 = @raw.gsub /\r/, ""
+      # d for delimiter
+      d = string64[(/MathType(.)MTEF/ =~ string64) + 8]
+      matches = string64.match /.*MathType#{d}MTEF#{d}(.)#{d}(.)#{d}.{2}(.*)#{d}([0-9A-F]{4})#{d}.*/m
+      leading_chrs = matches[1].to_i
+      trailing_chrs = matches[2].to_i - 1
+      string64   = matches[3]
+      mtchecksum = matches[4]
+      string64.gsub! /(\n).{#{leading_chrs}}/, "\n"
+      string64.gsub! /.{#{trailing_chrs}}\n/, ''
       @equation = decode string64
-      raise ArgumentError, "Checksums do not match, expected #{mtchecksum} but got #{checksum}" unless checksum == mtchecksum
+      unless checksum == mtchecksum
+        raise ArgumentError, "Checksums do not match, expected #{mtchecksum} but got #{checksum}"
+      end
     end
   end
 end
