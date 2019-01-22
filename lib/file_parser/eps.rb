906739ccf70b0e1bc00376d784910b80167c0c96
Sebastian Bulka <sebastian.bulka@le-tex.de>
2019-01-22 15:56:01 +0100

    simplify calls

--- lib/file_parser/eps.rb
+++ lib/file_parser/eps.rb
@@ -8,14 +8,13 @@ module Mathtype
     def extract_mtef_from_epscomment(comment)
       string64 = @raw.gsub /\r/, ""
       # d for delimiter
-      d = string64[(/MathType(.)MTEF/ =~ string64) + 8]
+      d = string64[(/MathType(.)MTEF\1/ =~ string64) + 8]
       matches = string64.match /.*MathType#{d}MTEF#{d}(.)#{d}(.)#{d}.{2}(.*)#{d}([0-9A-F]{4})#{d}.*/m
       leading_chrs = matches[1].to_i
       trailing_chrs = matches[2].to_i - 1
       string64   = matches[3]
       mtchecksum = matches[4]
-      string64.gsub! /(\n).{#{leading_chrs}}/, "\n"
-      string64.gsub! /.{#{trailing_chrs}}\n/, ''
+      string64.gsub! /.{#{trailing_chrs}}(\n).{#{leading_chrs}}/m, ""
       @equation = decode string64
       unless checksum == mtchecksum
         raise ArgumentError, "Checksums do not match, expected #{mtchecksum} but got #{checksum}"
